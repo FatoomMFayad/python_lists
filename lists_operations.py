397e50c80f96d4ef14bb20f1e2cafa091ff5026e
ffayad <ffayad@ptcdb.edu.ps>
2025-01-01 09:05:02 +0200

    reverse the list using reversed function

--- lists_operations.py
+++ lists_operations.py
@@ -1,6 +1,10 @@
 x = [1, 5, 3, 5, 9, 8, 5]
 # count number of occurrences of 5 in x
 print(x.count(5))
-# reverse the array
+# reverse the list using reverse function
 x.reverse()
 print(x)
+
+# reverse the list using reversed function'
+y = list(reversed(x))
+print(y)
