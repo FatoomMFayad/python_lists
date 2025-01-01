d578c3d3ed0b70c39fb8a5ec87149dd09f8cfe29
ffayad <ffayad@ptcdb.edu.ps>
2025-01-01 14:06:48 +0200

    extract years and store it using list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -5,3 +5,11 @@ print(salaries2)
 names = ['Fatoom', 'Mohammed', '', 'Fayad']
 names2=[name for name in names if name!='']
 print(names2)
+
+x = [1, 5, 8]
+y = [i**2 for i in x]
+print(y)
+
+dates = ['01-01-2020', '05-05-2022', '06-08-2023','04-10-2019']
+years = [int(d[-4:]) for d in dates]
+print(years)
