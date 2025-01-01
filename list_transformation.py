5b5c7688b7029922080ee08e581ebc3b7392341e
ffayad <ffayad@ptcdb.edu.ps>
2025-01-01 14:01:26 +0200

    short append with if statement

--- list_transformation.py
+++ list_transformation.py
@@ -1,3 +1,7 @@
 salaries = ['$400', '$5000', '$6000']
 salaries2 = [int(i[1:]) for i in salaries]
 print(salaries2)
+
+names = ['Fatoom', 'Mohammed', '', 'Fayad']
+names2=[name for name in names if name!='']
+print(names2)
