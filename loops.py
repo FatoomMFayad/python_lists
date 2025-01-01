0e069d1e3285fde71581d6a8b8caa08a663f21e0
ffayad <ffayad@ptcdb.edu.ps>
2025-01-01 09:20:08 +0200

    salaries example

--- loops.py
+++ loops.py
@@ -2,3 +2,11 @@ x = [1, 5, 5, 8, 7, 9, 44]
 
 for i in x:
     print(i**2)
+
+
+#slaries tax
+salaries = [5000, 1500, 2500, 4000]
+net_salaries = []
+for salary in salaries:
+    net_salaries.append(salary - 0.05*salary)
+print(net_salaries)
