4252d26be1653e1c95abfc63ffa723c83c1e324c
ffayad <ffayad@ptcdb.edu.ps>
2025-01-01 14:08:35 +0200

    if else in list comperhension

--- list_comprehension.py
+++ list_comprehension.py
@@ -13,3 +13,7 @@ print(y)
 dates = ['01-01-2020', '05-05-2022', '06-08-2023','04-10-2019']
 years = [int(d[-4:]) for d in dates]
 print(years)
+
+names_alt = ['Fatoom', 'Mohammed', '', 'Fayad']
+names3=[name if name!='' else 'Unknown' for name in names ]
+print(names3)
